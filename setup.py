aaf38388c840d1be7891369aae1d8a214103fd82
nordmenss <nordmenss@gmail.com>
2011-02-02 19:39:47 +0200

    Ошибка исправлена

--- setup.py
+++ setup.py
@@ -1,7 +1,28 @@
-# -*- coding: utf-8 -*-
-from setuptools import setup, find_packages
+#!/usr/bin/env python
+from distutils.core import setup
+import os
+path='content-base'
+from path import VERSION
 
 long_description = open('README.txt').read()
+packages, data_files = [], []
+root_dir = os.path.dirname(__file__)
+if root_dir:
+    os.chdir(root_dir)
+
+for dirpath, dirnames, filenames in os.walk(path):
+    # Ignore dirnames that start with '.'
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.'): del dirnames[i]
+    if '__init__.py' in filenames:
+        pkg = dirpath.replace(os.path.sep, '.')
+        if os.path.altsep:
+            pkg = pkg.replace(os.path.altsep, '.')
+        packages.append(pkg)
+    elif filenames:
+        prefix = dirpath[(len(path)+1):] # Strip "admin_tools/" or "admin_tools\"
+        for f in filenames:
+            data_files.append(os.path.join(prefix, f))
 
 setup(
     name='django-content-base',
@@ -11,7 +32,9 @@ setup(
     author='NORD',
     author_email='nordmenss@gmail.com',
     url='https://github.com/nordmenss/django-content-base',
-    packages=find_packages(),
+    package_dir={'content-base': 'content-base'},
+    packages=packages,
+    package_data={'content-base': data_files},
     classifiers=[
         'Development Status :: 0 - Alpha',
         'Environment :: Web Environment',
@@ -21,5 +44,6 @@ setup(
         'Programming Language :: Python',
         'Framework :: Django',
     ],
+    install_requires=[],
     zip_safe=False,
 )
