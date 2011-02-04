ace3e38d54d8edc7fa3344ea7b3ade97cffb5622
nordmenss <nordmenss@gmail.com>
2011-02-04 11:53:52 +0200

    new

--- setup.py
+++ setup.py
@@ -1,19 +1,52 @@
 #!/usr/bin/env python
-from setuptools import setup, find_packages
+from distutils.core import setup
+import os
+from distutils.sysconfig import get_python_lib
 from content_base import VERSION
 long_description = open('README.txt').read()
+path='content_base'
+dir=filename=get_python_lib()+"/"+path
+if os.path.exists(dir)==False:
+    os.mkdir(dir)
+
+packages, data_files = [], []
+root_dir = os.path.dirname(__file__)
+if root_dir:
+    os.chdir(root_dir)
+    print "root_dir=",root_dir
+
+for dirpath, dirnames, filenames in os.walk(path):
+    print "filenames=",filenames
+    # Ignore dirnames that start with '.'
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.'): del dirnames[i]
+    if '__init__.py' in filenames:
+        pkg = dirpath.replace(os.path.sep, '.')
+        if os.path.altsep:
+            pkg = pkg.replace(os.path.altsep, '.')
+        print "pkg=",pkg
+        packages.append(pkg)
+    elif filenames:
+        prefix = dirpath[(len(path)+1):] # Strip "admin_tools/" or "admin_tools\"
+        for f in filenames:
+            filename=get_python_lib()+"/"+path+"/"+f
+            #data_files.append(filename)
+            filename=os.path.join(prefix, f)
+            print "filename=",filename
+            data_files.append(os.path.join(prefix, f))
+
+os.chdir(dir)
 
 setup(
     name='django-content-base',
-    version=VERSION,
+    version='0.0',
     description='content abstract class',
     long_description=long_description,
     author='NORD',
     author_email='nordmenss@gmail.com',
     url='https://github.com/nordmenss/django-content-base',
-    packages=find_packages(),
-    include_package_data=True,
-    zip_safe=False,
+    package_dir={path: path},
+    package_data={path: data_files},
     classifiers=[
         'Development Status :: 0 - Alpha',
         'Environment :: Web Environment',
@@ -24,4 +57,5 @@ setup(
         'Framework :: Django',
     ],
     install_requires=[],
+    zip_safe=False,
 )
